3d5d4a492e6564c0bb8b390f66f3814a7f1dfd39
unknown <sashabasiuk1908@gmail.com>
2024-01-11 21:09:24 +0200

    logo component

---
+++ my-app/src/components/logo/logo.jsx
@@ -0,0 +1,18 @@
+import './logo.css'
+import logo from '../img/logo-g-color@2x.png'
+
+const Logo = () => {
+    return (
+       <div className="logo">
+            <img src={logo} alt="logo" />
+            <ul>
+                <li>Послуги</li>
+                <li>Про мене</li>
+                <li>Портфоліо</li>
+                <li>Контакти</li>
+            </ul>
+       </div>
+    );
+};
+
+export default Logo;
