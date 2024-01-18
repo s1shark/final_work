b6b7410a74498c6d95c8b8ce182589afff715b1a
unknown <sashabasiuk1908@gmail.com>
2024-01-18 20:46:22 +0200

    services component

---
+++ my-app/src/components/Services/Services.jsx
@@ -0,0 +1,16 @@
+import './services.css'
+import Header from '../header/header';
+
+const Services = () => {
+    return (
+        <>
+            <Header />
+            <h1>Послуги</h1>
+            <div className='page'>
+
+            </div>
+        </>
+    );
+};
+
+export default Services;
