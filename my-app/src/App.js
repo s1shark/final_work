5e7d1405d9dbacf7d0d99090acb17788a402a654
unknown <sashabasiuk1908@gmail.com>
2024-01-23 19:02:56 +0200

    services component completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div>
       {/* <Header />
-      <Portfolio /> */}
-      <Services />
+      <Portfolio />  */}
+      <Services /> 
     </div>
   );
 }
--- my-app/src/components/Services/Services.jsx
+++ my-app/src/components/Services/Services.jsx
@@ -1,5 +1,8 @@
 import './services.css'
 import Header from '../header/header';
+import img1 from "../img/KET00260.jpg"
+import img2 from "../img/KET03537+.jpg"
+import img3 from "../img/KET05990.jpg"
 
 const Services = () => {
     return (
@@ -7,8 +10,17 @@ const Services = () => {
             <Header />
             <h1>Послуги</h1>
             <div className='page'>
-
+                <ul>
+                    <li><img className='img1' src={img1} alt="" /></li>
+                    <li><img className='img2' src={img2} alt="" /></li>
+                    <li><img className='img3' src={img3} alt="" /></li>
+                </ul>
             </div>
+            <p className='p'>ціни розраховуються індивідуально в залежності від складності, для цього потрібно <br/>заповнити форму</p>
+            <div className="service">
+                <button className='service-button'>форма замовлення</button>
+            </div> 
+            <div className='end'>Back to Top</div>
         </>
     );
 };
