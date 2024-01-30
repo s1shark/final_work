232b010aa2433bf7a21f752df6798253a1ad29de
unknown <sashabasiuk1908@gmail.com>
2024-01-30 16:03:40 +0200

    component done

--- my-app/src/components/Contact/Contact.jsx
+++ my-app/src/components/Contact/Contact.jsx
@@ -1,24 +1,46 @@
 import './contact.css'
 import Header from '../header'
+import React, { useState } from 'react';
 
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        massage: 'hi test'
+    });
+
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('Submitted', formData);
+        setIsSubmitted(true);
+    };
+
     return (
-        <>
+        <div className='div-form'>
             <Header />
             <h1>Контакти</h1>
-            <div className='div-form'>
-                <form action="#" id='form'>
-                    <label>Name*</label><br/>
-                    <input type="text" value="Your name..." /><br/>
-                    <label>Email Address *</label><br/>
-                    <input type="text" value="Your email address..." /><br/>
-                    <label>Message*</label><br/>
-                    <textarea name="" id="" cols="30" rows="10" value="Your message..."></textarea><br/>
+            <form onSubmit={handleSubmit} >
+                <label>Name *</label><br/>
+                <input type="text" name='name' value={formData.name} onChange={handleChange} required /><br/>
+                <label>Email *</label><br/>
+                <input type="email" name='email' value={formData.email} onChange={handleChange} required /><br/>
+                <label>Message</label><br/>
+                <textarea name="message" value={formData.massage} onChange={handleChange} ></textarea><br/>
 
-                    <button className='form-button'>Відправити</button>
-                </form>
-            </div>   
-        </> 
+                <button type='submit' className='contact-button'>Відправити</button>
+            </form>
+        </div>
     );
 };
 
